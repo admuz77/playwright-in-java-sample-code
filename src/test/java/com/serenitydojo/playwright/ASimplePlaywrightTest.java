77139ee6e2bcc96960628c30766ff19185f0a3af
AnnaDK <annadktester@gmail.com>
2025-09-15 10:43:52 +0200

    refactoring

--- src/test/java/com/serenitydojo/playwright/ASimplePlaywrightTest.java
+++ src/test/java/com/serenitydojo/playwright/ASimplePlaywrightTest.java
@@ -1,15 +1,31 @@
 package com.serenitydojo.playwright;
 
-import com.microsoft.playwright.Browser;
 import com.microsoft.playwright.Page;
-import com.microsoft.playwright.Playwright;
+import com.microsoft.playwright.junit.UsePlaywright;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+@UsePlaywright
 public class ASimplePlaywrightTest {
 
     @Test
-    void shouldShowThePageTitle() {
-        // TODO: Write your first playwright test here
+    void shouldShowThePageTitle(Page page) {
+
+        page.navigate("https://practicesoftwaretesting.com");
+        String title = page.title();
+        Assertions.assertTrue(title.contains("Practice Software Testing"));
+    }
+
+
+    @Test
+    void searchAndAssertVisibilityOfSearchTermsInTitle(Page page) {
+
+        page.navigate("https://practicesoftwaretesting.com");
+        page.locator("[placeholder=Search]").fill("Pilers");
+        page.locator("button:has-text('Search')").click();
+
+        int matchingProducCount = page.locator(".card-title").count();
+
+        Assertions.assertTrue(matchingProducCount > 0);
     }
 }
